b209b1329e3492ebc1c81247f08fdb0bc6778f59
Youngjin Jo <yjinjo@berkeley.edu>
2023-04-27 17:26:14 +0900

    #1. path parameter 처리 및 GET, POST 메서드, 쿼리 스트링 테스트

--- shortener/views.py
+++ shortener/views.py
@@ -1,4 +1,6 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 
 from shortener.models import Users
 
@@ -11,6 +13,26 @@ def index(request):
     return render(request, "base.html", {"welcome_msg": f"Hello {email}"})
 
 
-def redirect_test(request):
-    print("Go Redirect")
-    return redirect("index")
+@csrf_exempt
+def get_user(request, user_id):
+    print(user_id)
+    if request.method == "GET":
+        abc = request.GET.get("abc")
+        xyz = request.GET.get("xyz")
+        user = Users.objects.filter(pk=user_id).first()
+        return render(
+            request,
+            "base.html",
+            {
+                "user": user,
+                "params": [abc, xyz],
+            },
+        )
+    elif request.method == "POST":
+        username = request.GET.get("username")
+        if username:
+            user = Users.objects.filter(pk=user_id).update(username=username)
+
+        return JsonResponse(
+            status=201, data=dict(msg="You just reached with Post Method!"), safe=False
+        )
--- shrinkers/urls.py
+++ shrinkers/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from shortener.views import index, redirect_test
+from shortener.views import index, get_user
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index, name="index"),
-    path("redirect", redirect_test),
+    path("get_user/<int:user_id>", get_user),
 ]
