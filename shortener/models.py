efdd0dca74ac287c3f5576031367e63516f0cf25
Youngjin Jo <yjinjo@berkeley.edu>
2023-04-27 12:58:07 +0900

    #1. PayPlan 모델 설계 및 Migrate

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class PayPlan(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
