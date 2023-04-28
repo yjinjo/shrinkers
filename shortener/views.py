7273ee159b0c86840b4033933ae6708b5c39c40f
Youngjin Jo <yjinjo@berkeley.edu>
2023-04-28 10:35:14 +0900

    #1. Django Form으로 게시판 만들기

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
@@ -5,8 +6,8 @@ from django.http.response import JsonResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 
-from shortener.forms import RegisterForm, LoginForm
-from shortener.models import Users
+from shortener.forms import RegisterForm, LoginForm, UrlCreateForm
+from shortener.models import Users, ShortenedUrls
 
 
 def index(request):
@@ -18,10 +19,54 @@ def index(request):
 
 
 def url_list(request):
-    return render(
-        request,
-        "url_list.html",
-    )
+    get_list = ShortenedUrls.objects.order_by("-created_at").all()
+    return render(request, "url_list.html", {"list": get_list})
+
+
+@login_required
+def url_create(request):
+    msg = None
+    if request.method == "POST":
+        form = UrlCreateForm(request.POST)
+        if form.is_valid():
+            msg = f"{form.cleaned_data.get('nick_name')} 생성 완료!"
+            messages.add_message(request, messages.INFO, msg)
+            form.save(request)
+            return redirect("url_list")
+        else:
+            form = UrlCreateForm()
+    else:
+        form = UrlCreateForm()
+    return render(request, "url_create.html", {"form": form})
+
+
+@login_required
+def url_change(request, action, url_id):
+    if request.method == "POST":
+        url_data = ShortenedUrls.objects.filter(id=url_id)
+        if url_data.exists():
+            if url_data.first().created_by_id != request.user.id:
+                msg = "자신이 소유하지 않은 URL 입니다."
+            else:
+                if action == "delete":
+                    msg = f"{url_data.first().nick_name} 삭제 완료!"
+                    url_data.delete()
+                    messages.add_message(request, messages.INFO, msg)
+                elif action == "update":
+                    msg = f"{url_data.first().nick_name} 수정 완료!"
+                    form = UrlCreateForm(request.POST)
+                    form.update_form(request, url_id)
+
+                    messages.add_message(request, messages.INFO, msg)
+        else:
+            msg = "해당 URL 정보를 찾을 수 없습니다."
+
+    elif request.method == "GET" and action == "update":
+        url_data = ShortenedUrls.objects.filter(pk=url_id).first()
+        form = UrlCreateForm(instance=url_data)
+        return render(request, "url_create.html", {"form": form, "is_update": True})
+
+    return redirect("url_list")
 
 
 @csrf_exempt
--- shrinkers/urls.py
+++ shrinkers/urls.py
@@ -10,6 +10,8 @@ from shortener.views import (
     logout_view,
     list_view,
     url_list,
+    url_create,
+    url_change,
 )
 
 urlpatterns = [
@@ -17,7 +19,9 @@ urlpatterns = [
     path("", index, name="index"),
     path("register", register, name="register"),
     path("login", login_view, name="login"),
-    path("url_list", url_list, name="url_list"),
+    path("urls", url_list, name="url_list"),
+    path("urls/create", url_create, name="url_create"),
+    path("urls/<str:action>/<int:url_id>", url_change, name="url_change"),
     path("logout", logout_view, name="logout"),
     path("list", list_view, name="list_view"),
     path("get_user/<int:user_id>", get_user),
